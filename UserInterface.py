c5fbd9a6e60e58c2e9f11bce66f6e27f2f875f69
BulletStrat <taufiqrx8@gmail.com>
2017-01-15 15:13:09 +0600

    Optimized code and removed several bugs.

--- UserInterface.py
+++ UserInterface.py
@@ -1,8 +1,9 @@
+                         # ***Created on 2016/11/08 by Taufiq***
+
 import os
 import sys
 import GraphVariables as GV
 from PyQt5 import QtCore, QtGui
-from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import (
                              QMainWindow,
                              QComboBox,
@@ -13,22 +14,25 @@ from PyQt5.QtWidgets import (
                              QLabel,
                              QRadioButton,
                              QMessageBox,
-                             QWidget
                              )
-from DataAnalyzer import GraphData
+from Logistics import (
+                             GraphData,
+                             LiveGraph
+                          )
 
 
 class Window(QMainWindow):
 
-
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 529, 300)
+        #QWidget.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint)
+        # self.QSize(529, 300)
         self.setWindowTitle("GraphPy 1.0")
         self.setWindowIcon(QtGui.QIcon('logo.png'))
         button = QPushButton('Choose File', self)
         button.setGeometry(QtCore.QRect(70, 20, 91, 31))
-        button.clicked.connect(self.showDialog)
+        button.clicked.connect(self.file_dialog)
         self.lineEdit = QLineEdit(self)
         self.lineEdit.setGeometry(QtCore.QRect(210, 20, 272, 31))
         self.lineEdit.setObjectName("lineEdit")
@@ -36,7 +40,7 @@ class Window(QMainWindow):
         label = QLabel(self)
         label.setGeometry(QtCore.QRect(70, 80, 91, 31))
         label.setObjectName("label")
-        self.radioButton = QRadioButton("    Grid",self)
+        self.radioButton = QRadioButton("    Grid", self)
         self.radioButton.setGeometry(QtCore.QRect(30, 120, 82, 21))
         self.radioButton.setObjectName("radioButton")
         self.cb = QComboBox(self)
@@ -50,93 +54,87 @@ class Window(QMainWindow):
         label_6.setGeometry(QtCore.QRect(115, 83, 47, 13))
         label_6.setObjectName("label_3")
         self.lineEdit_2 = QLineEdit(self)
-        self.lineEdit_2.setGeometry(QtCore.QRect(240, 120, 113, 20))
+        self.lineEdit_2.setGeometry(QtCore.QRect(270, 120, 100, 20))
         self.lineEdit_2.setObjectName("lineEdit_2")
-        #self.lineEdit_2.setText("blank")
         self.lineEdit_3 = QLineEdit(self)
-        self.lineEdit_3.setGeometry(QtCore.QRect(370, 120, 113, 20))
+        self.lineEdit_3.setGeometry(QtCore.QRect(382, 120, 100, 20))
         self.lineEdit_3.setObjectName("lineEdit_3")
         self.lineEdit_4 = QLineEdit(self)
-        self.lineEdit_4.setGeometry(QtCore.QRect(110, 120, 113, 20))
+        self.lineEdit_4.setGeometry(QtCore.QRect(157, 120, 100, 20))
         self.lineEdit_4.setObjectName("lineEdit_4")
-        self.label_2 = QLabel("    Title",self)
-        self.label_2.setGeometry(QtCore.QRect(140, 141, 47, 13))
+        self.lineEdit_5 = QLineEdit(self)
+        self.lineEdit_5.setGeometry(QtCore.QRect(93, 120, 53, 20))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.label_2 = QLabel("    Title", self)
+        self.label_2.setGeometry(QtCore.QRect(187, 141, 47, 13))
         self.label_2.setObjectName("label_2")
-        label_3 = QLabel(" X-Label",self)
-        label_3.setGeometry(QtCore.QRect(270, 141, 47, 13))
+        label_3 = QLabel(" X-Label", self)
+        label_3.setGeometry(QtCore.QRect(300, 141, 47, 13))
         label_3.setObjectName("label_3")
-        label_4 = QLabel(" Y-Label",self)
-        label_4.setGeometry(QtCore.QRect(400, 141, 47, 13))
+        label_4 = QLabel(" Y-Label", self)
+        label_4.setGeometry(QtCore.QRect(412, 141, 47, 13))
         label_4.setObjectName("label_4")
-        self.pushButton_2 = QPushButton("Graph Data",self)
-        self.pushButton_2.setGeometry(QtCore.QRect(335, 200, 150, 61))
+        label_5 = QLabel("Threshold", self)
+        label_5.setGeometry(QtCore.QRect(96, 141, 47, 13))
+        label_5.setObjectName("label_4")
+        self.pushButton_2 = QPushButton("Graph Data", self)
+        self.pushButton_2.setGeometry(QtCore.QRect(355, 170, 130, 61))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_2.clicked.connect(self.clickGraph)
+        self.pushButton_2.clicked.connect(self.click_graph)
         self.pushButton_3 = QPushButton("Preview File", self)
-        self.pushButton_3.setGeometry(QtCore.QRect(180, 200, 150, 61))
+        self.pushButton_3.setGeometry(QtCore.QRect(217, 170, 130, 61))
         self.pushButton_3.setObjectName("pushButton_3")
         self.pushButton_3.clicked.connect(self.preview)
         self.pushButton_4 = QPushButton("Live Mode", self)
-        self.pushButton_4.setGeometry(QtCore.QRect(70, 200, 150, 61))
+        self.pushButton_4.setGeometry(QtCore.QRect(78, 170, 130, 61))
         self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_4.clicked.connect(self.buildExamplePopup())
-
-
+        self.pushButton_4.clicked.connect(self.live_graph)
+        self.status = self.statusBar()
+        self.status.showMessage('')
         self.show()
 
-    @pyqtSlot()
-    def choose_file(self):
-            print('PyQt5 button click')
-
-    @pyqtSlot()
-    def showDialog(self):
+    def file_dialog(self):
         self.cb.clear()
         self.cb_2.clear()
-        fname = QFileDialog.getOpenFileName(self, 'Choose file', '',("files (*.txt *.csv *.xlsx *.xlrs *.xlr )"))
-        self.file  = fname[0]
-
+        f_name = QFileDialog.getOpenFileName(self, 'Choose file', 'g', ("files (*.txt *.csv *.xlsx *.xlrs *.xlr )"))
+        self.file  = f_name[0]
         print(self.file)
         self.lineEdit.setText(self.file)
         self.combo(self.file)
+        self.status.showMessage('File selected')
+
+    def click_graph(self):
+        if self.radioButton.isChecked():
+            print("grid:true")
+            grid = True
+        else:
+            print("grid:false")
+            grid = False
+        self.threshold = self.lineEdit_5.text()
+        print(self.lineEdit_4.text())  # title
+        print(self.lineEdit_2.text())  # x
+        print(self.lineEdit_3.text())  # y
+        print(int(self.lineEdit_5.text()))  # threshold
+        print(str(self.get_x))
+        print(str(self.get_y))
+        gd = GraphData(
+            self.file,
+            self.lineEdit_4.text(),
+            self.lineEdit_2.text(),
+            self.lineEdit_3.text(),
+            grid,
+            self.get_x,
+            self.get_y,
+            float(self.lineEdit_5.text())
+        )
+
+        gd.file_type()
 
 
-
-    @pyqtSlot()
-    def clickGraph(self):
-         if self.radioButton.isChecked():
-             print("grid:true")
-             grid = True
-         else:
-             print("grid:false")
-             grid = False
-
-         print(self.lineEdit_4.text()) #title
-         print(self.lineEdit_2.text()) #x
-         print(self.lineEdit_3.text()) #y
-         #if self.file.lower().endswith(('.txt', '.csv')):
-             #CSV_Handler.__init__(self.file)
-         print(str(self.get_x))
-         print(str(self.get_y))
-         gd = GraphData(self.file,self.lineEdit_4.text(),
-                        self.lineEdit_2.text(),
-                        self.lineEdit_3.text(),
-                        grid,self.get_x,
-                        self.get_y)
-         gd.file_type()
-
-         #graphdata.graph()
-         # elif self.file.lower().endswith(('.xlrs', '.xlr', '.xlsx')):
-         #     Excel_Handler.__init__(self.file)
-         # else:
-         #     print("Unsupported Filetype!")
-
-    @pyqtSlot()
     def preview(self):
         print(self.file)
         os.system('"'+(self.file)+'"')
 
-
-    @pyqtSlot()
     def combo(self,file):
         print("combo function triggered")
         print(file)
@@ -145,50 +143,33 @@ class Window(QMainWindow):
             self.cb.addItem(vars[i])
             self.cb_2.addItem(vars[i])
             print(vars[i])
-        self.cb.currentIndexChanged.connect(self.ComboSelected)
-        self.cb_2.currentIndexChanged.connect(self.ComboSelected)
+        self.cb.currentIndexChanged.connect(self.combo_selected)
+        self.cb_2.currentIndexChanged.connect(self.combo_selected)
 
-    @pyqtSlot()
-    def ComboSelected(self):
+    def combo_selected(self):
         x =str(self.cb.currentText())
-        print ("x= ",x)
+        print ("x= ", x)
         y = str(self.cb_2.currentText())
-        print("y= ",y)
+        print("y= ", y)
         self.get_x = x
         self.get_y = y
 
-
-    def get_x(self,x):
+    def get_x(self, x):
         return x
 
-    def get_y(self,y):
+    def get_y(self, y):
         return y
 
-    def showmessage(self,message):
+    def live_graph(self):
+        lv = LiveGraph(self.file)
+        lv.showupdate()
+
+    def showmessage(self, message):
         msg = self.QMessageBox()
         msg.setIcon(self.QMessageBox.Information)
-
         msg.setText(message)
         msg.setWindowTitle("Error!")
         msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
-        #msg.buttonClicked.connect(msgbtn)
-
-    def buildExamplePopup(self):
-        name ="POPUP"
-        self.exPopup = examplePopup(name)
-        self.exPopup.setGeometry(100, 200, 100, 100)
-        self.exPopup.show()
-
-class examplePopup(QWidget):
-    def __init__(self, name):
-        super().__init__()
-
-        self.name = name
-
-        self.initUI()
-
-    def initUI(self):
-        lblName = QLabel(self.name, self)
 
 app = QApplication(sys.argv)
 GUI = Window()
