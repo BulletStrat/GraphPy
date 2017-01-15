8854ff399e0be7106a717e83952a8827a53f5fa5
BulletStrat <taufiqrx8@gmail.com>
2017-01-15 15:07:41 +0600

    Update GraphVariables.py

--- GraphVariables.py
+++ GraphVariables.py
@@ -1,25 +1,13 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 
-# file ='World Population Growth.csv'
-# print("Is your data separated by commas or empty spaces?Please specify with characters:")
-# delimiter = str(input())
-# df = pd.read_csv(file, sep=delimiter, header=0, engine='python')
-# print(df)
-# pd.crosstab(df["year"],df["population"],margins=True)
-# plt.plot(df["year"],df["population"])
-# plt.grid(True)
-# plt.show()
 def read_vars(file):
-
     df = pd.read_csv(file, sep=',', header=0, engine='python')
     vars = list(df)
     return vars
 
-
 def read_vars_beta(file):
     with open(file) as f:
         data = f.readline()
-    vars = data.strip().split(',')
+    vars = data.strip().split( ',')
     print (vars)
     return vars
